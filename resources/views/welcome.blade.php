2002d0e08ccbc1bf73270367a282c8fefb040f81
johndoedc-tech <delacruzjeydi16@gmail.com>
2026-03-29 12:20:43 +0800

    edited title 'benguetcropmap' to 'geomap'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BenguetCropMap - GIS-Integrated DSS</title>
+    <title>GeoMap - GIS-Integrated DSS</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <style>
         * {
